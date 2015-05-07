63a5904d6c2129c8869ee0aa6931a0f342dc7960
Kevin Roark <ker2143@columbia.edu>
2015-05-07 06:27:17 -0400

    update url config

--- js/build/build.js
+++ js/build/build.js
@@ -5475,11 +5475,6 @@ Object.defineProperty(exports, "__esModule", {
 },{"jquery":22,"three":24}],19:[function(require,module,exports){
 "use strict";
 
-module.exports.godIsAMan = {
-  web: "http://kevin-roark.github.io/second-shane-god-man/",
-  live: "http://localhost:5555/"
-};
-
 module.exports.asmr = {
   web: "http://kevin-roark.github.io/second-shane-asmr/",
   live: "http://localhost:5555/"
@@ -5487,17 +5482,22 @@ module.exports.asmr = {
 
 module.exports.liveAtJJs = {
   web: "http://kevin-roark.github.io/second-shane-live-at-jjs/",
-  live: "http://localhost:5555/"
+  live: "http://localhost:5556/"
 };
 
 module.exports.iFeltTheFoot = {
   web: "http://kevin-roark.github.io/second-shane-i-felt-the-foot/",
-  live: "http://localhost:5555/"
+  live: "http://localhost:5557/"
 };
 
 module.exports.papaJohn = {
   web: "http://kevin-roark.github.io/second-shane-papa-john/",
-  live: "http://localhost:5555/"
+  live: "http://localhost:5558/"
+};
+
+module.exports.godIsAMan = {
+  web: "http://kevin-roark.github.io/second-shane-god-man/",
+  live: "http://localhost:5559/"
 };
 
 },{}],20:[function(require,module,exports){
--- js/urls.js
+++ js/urls.js
@@ -1,9 +1,4 @@
 
-module.exports.godIsAMan = {
-  web: 'http://kevin-roark.github.io/second-shane-god-man/',
-  live: 'http://localhost:5555/'
-};
-
 module.exports.asmr = {
   web: 'http://kevin-roark.github.io/second-shane-asmr/',
   live: 'http://localhost:5555/'
@@ -11,15 +6,20 @@ module.exports.asmr = {
 
 module.exports.liveAtJJs = {
   web: 'http://kevin-roark.github.io/second-shane-live-at-jjs/',
-  live: 'http://localhost:5555/'
+  live: 'http://localhost:5556/'
 };
 
 module.exports.iFeltTheFoot = {
   web: 'http://kevin-roark.github.io/second-shane-i-felt-the-foot/',
-  live: 'http://localhost:5555/'
+  live: 'http://localhost:5557/'
 };
 
 module.exports.papaJohn = {
   web: 'http://kevin-roark.github.io/second-shane-papa-john/',
-  live: 'http://localhost:5555/'
+  live: 'http://localhost:5558/'
+};
+
+module.exports.godIsAMan = {
+  web: 'http://kevin-roark.github.io/second-shane-god-man/',
+  live: 'http://localhost:5559/'
 };
